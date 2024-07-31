22154bcd62630c1209d5c01372565baf51dc451a
vinodhnartu120940 <vinodhnartu120940@gmail.com>
2024-07-31 21:54:52 +0530

    sensor data changes

--- src/app/user/Pages/revenue/revenue.component.ts
+++ src/app/user/Pages/revenue/revenue.component.ts
@@ -1,22 +1,19 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder, FormsModule } from '@angular/forms';
+import {
+  FormGroup,
+  FormControl,
+  FormBuilder,
+  FormsModule,
+} from '@angular/forms';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-
 
 @Component({
   selector: 'app-revenue',
   standalone: true,
   imports: [FormsModule],
   templateUrl: './revenue.component.html',
-  styleUrl: './revenue.component.scss'
+  styleUrl: './revenue.component.scss',
 })
 export class RevenueComponent {
-
-
- 
-  
-  openAddRevenueModal() {
-   
-  }
-  
+  openAddRevenueModal() {}
 }
