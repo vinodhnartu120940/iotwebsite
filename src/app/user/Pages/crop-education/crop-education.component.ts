9703581ffb82a49c0fd9b75c40aeb11e766def82
harshadatta099 <harshaa@smbxl.com>
2024-08-13 16:40:24 +0530

    changes!

--- src/app/user/Pages/crop-education/crop-education.component.ts
+++ src/app/user/Pages/crop-education/crop-education.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { UserService } from '../../user.service';
+import { NgClass, NgForOf } from '@angular/common';
 
 @Component({
   selector: 'app-crop-education',
   standalone: true,
-  imports: [],
+  imports: [NgClass,NgForOf],
   templateUrl: './crop-education.component.html',
   styleUrl: './crop-education.component.scss'
 })
 export class CropEducationComponent {
+  cropData:any;
 
+  constructor(private userService:UserService){
+    this.userService.GetAgronomicPractices().subscribe((res)=>{
+      this.cropData = res;
+    })
+  }
 }
